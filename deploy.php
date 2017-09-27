289612b78f6a2782e536d0cafcb0291fb4872f11
BornaSmit <smit.borna@gmail.com>
2017-09-27 22:22:18 +0200

    Fix deploy.php

--- deploy.php
+++ deploy.php
@@ -5,6 +5,18 @@
      * Used for automatically deploying websites via GitHub
      *
      */
+    
+    if (!function_exists('getallheaders')) {
+        function getallheaders() {
+            $headers = [];
+            foreach ($_SERVER as $name => $value) {
+                if (substr($name, 0, 5) == 'HTTP_') {
+                    $headers[str_replace(' ', '-', ucwords(strtolower(str_replace('_', ' ', substr($name, 5)))))] = $value;
+                }
+            }
+            return $headers;
+        }
+    }
 
     $secret = $SECRET_TOKEN;
 
